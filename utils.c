eda8c9f6494be0e042ad9e659f390d18c70b9f35
Jubile <erfahabib@gmail.com>
2017-01-26 05:40:38 -0500

    fixed seg fault

--- utils.c
+++ utils.c
@@ -8,11 +8,18 @@ typedef struct record {
 } Record;
 
 Record * make_record(char *str){
+  printf("this should print %s\n", str);
+  if (str[0] == '\0') {
+	printf("empty string\n");
+  }
   char * val1 = strtok(str, ",");
+  printf("after filling val 1\n");
   char * val2 = strtok(NULL, ",");
+  printf("after filling val 2, val: %s\n", val2);
   int intval1 = atoi(val1);
+  printf("after filling intval1\n");
   int intval2 = atoi(val2);
-  
+  printf("making the record after this\n");
   Record * record = malloc(sizeof(Record));
   record->uid1 = intval1;
   record->uid2 = intval2;
@@ -37,14 +44,18 @@ void read_and_buffer(char *file_name, int blocksize){
       printf("Error: could not open file for write.");
       exit(1);
   }
-  
+  printf("before while loop\n");
   while (fgets(current_line, 1024, fp_read) != NULL) {
     current_line[strcspn(current_line, "\r\n")] = '\0';
+    printf("before making record\n");
     Record * record = make_record(current_line);
-    if (total_records < records_per_block ){
-      memcpy(buffer + (total_records * sizeof(Record)), record, sizeof(Record));
+    printf("after making record, before if statement\n");
+    if (total_records < records_per_block && current_line[0] != '\0'){
+      printf("right before memcpy\n");
+      memcpy(buffer + total_records, record, sizeof(Record));
       free(record);
       total_records += 1;
+      printf("total records: %d\n", total_records);
     } else {
       fwrite(buffer, sizeof(Record), total_records, fp_write);
       fflush(fp_write);
@@ -52,7 +63,11 @@ void read_and_buffer(char *file_name, int blocksize){
     }
     
   }
-  
+  if (total_records > 0) {
+	fwrite(buffer, sizeof(Record), total_records, fp_write);
+        fflush(fp_write);
+        total_records = 0;
+  }  
   fclose(fp_write);
   free(buffer);
   fclose(fp_read);
