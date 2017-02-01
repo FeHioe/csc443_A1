7738816730144a7b913fd802039caff7b1b88b8f
Habib Erfa <habiberf@cdf.toronto.edu>
2017-01-31 20:42:24 -0500

    added stuff to script

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@
 make
 
 filename=./data/edges.csv
+echo "first experiment"
 ./write_blocks_seq $filename 512
 ./write_blocks_seq $filename 1024
 ./write_blocks_seq $filename 4096
@@ -13,3 +14,57 @@ filename=./data/edges.csv
 ./write_blocks_seq $filename 2097152
 ./write_blocks_seq $filename 4194304
  
+cp $filename ./cp1.csv
+filename=cp1.csv
+echo "second experiment"
+./write_blocks_seq $filename 512
+./write_blocks_seq $filename 1024
+./write_blocks_seq $filename 4096
+./write_blocks_seq $filename 8192
+./write_blocks_seq $filename 16384
+./write_blocks_seq $filename 32768
+./write_blocks_seq $filename 1048576
+./write_blocks_seq $filename 2097152
+./write_blocks_seq $filename 4194304
+
+cp $filename ./cp2.csv
+filename=cp2.csv
+echo "third experiment"
+./write_blocks_seq $filename 512
+./write_blocks_seq $filename 1024
+./write_blocks_seq $filename 4096
+./write_blocks_seq $filename 8192
+./write_blocks_seq $filename 16384
+./write_blocks_seq $filename 32768
+./write_blocks_seq $filename 1048576
+./write_blocks_seq $filename 2097152
+./write_blocks_seq $filename 4194304
+
+cp $filename ./cp3.csv
+filename=cp3.csv
+echo "fourth experiment"
+./write_blocks_seq $filename 512
+./write_blocks_seq $filename 1024
+./write_blocks_seq $filename 4096
+./write_blocks_seq $filename 8192
+./write_blocks_seq $filename 16384
+./write_blocks_seq $filename 32768
+./write_blocks_seq $filename 1048576
+./write_blocks_seq $filename 2097152
+./write_blocks_seq $filename 4194304
+
+cp $filename ./cp4.csv
+filename=cp4.csv
+echo "fifth experiment"
+./write_blocks_seq $filename 512
+./write_blocks_seq $filename 1024
+./write_blocks_seq $filename 4096
+./write_blocks_seq $filename 8192
+./write_blocks_seq $filename 16384
+./write_blocks_seq $filename 32768
+./write_blocks_seq $filename 1048576
+./write_blocks_seq $filename 2097152
+./write_blocks_seq $filename 4194304
+
+
+
