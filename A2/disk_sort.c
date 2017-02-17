5e4935829d6f8b92bac9e7dfb8e02f4b7b998408
FeHioe <fe.hioe@gmail.com>
2017-02-17 01:05:48 -0500

    test

--- A2/disk_sort.c
+++ A2/disk_sort.c
@@ -107,11 +107,10 @@ int main(int argc, char *argv[]){
       num_block++;
 
       //printf("read\n");
-      /*
-      if (num_block == (chunk_size/block_size) && (chunk_size % block_size != 0)){
+      if (num_block+1 == (chunk_size/block_size) && (chunk_size % block_size != 0)){
         block_elements = (chunk_size % block_size) / sizeof(Record);
       };
-      */
+
       int y;
       for (y=0; y < 9; y++){
         printf ("block_buffer element: %d\n", block_buffer[y].UID2);
