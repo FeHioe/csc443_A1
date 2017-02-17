007b8536d58066843624d1787e93a15db97dca1d
FeHioe <fe.hioe@gmail.com>
2017-02-16 23:26:00 -0500

    typo

--- A2/disk_sort.c
+++ A2/disk_sort.c
@@ -96,11 +96,11 @@ int main(int argc, char *argv[]){
 
     int num_block = 0;
     int block_elements = block_size / sizeof(Record);
-    int test = ((int)chunk_size/block_size));
+    int test = ((int)chunk_size/block_size);
     printf("before read\n");
     printf("chunk: %d block: %d test:%d\n", chunk_size, block_size, test);
     while ( (result = fread(block_buffer, sizeof(Record), block_elements, fp_read) > 0) 
-      && (num_block <= test ){
+      && (num_block <= test ) ){
       num_block++;
 
       printf("read\n");
