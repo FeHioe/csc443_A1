d9e205a88a3749c2c7e22a045180615129ec80b1
FeHioe <fe.hioe@gmail.com>
2017-02-28 19:46:45 -0500

    compile fix

--- A2/distribution.c
+++ A2/distribution.c
@@ -1,138 +1,20 @@
 #include "merge.h"
+#include "disk_sort.h"
 
-int main(int argc, char *argv[]){
-	char *filename = argv[1];
-	int block_size = atoi(argv[2]);
-	int column_id = atoi(argv[3]);
-	int max_degree;
-	if (column_id == 1) {
-		max_degree = compute_max_out_degree(filename, block_size);
-	} else {
-		max_degree = compute_max_in_degree(filename, blocksize);	
-	}
-	distribution(filename, blocksize, column_id, max_degree);
-	return 0;
-}
-
-int compute_max_in_degree(char *filename, int block_size) {
-	int records_per_block = block_size/sizeof(Record);
-	FILE *fp_read;
-
-   long total_records = 0;
-
-	Record * buffer = (Record *) calloc (records_per_block, sizeof(Record));
-	if (!(fp_read = fopen(filename, "rb"))) {
-    	printf("Error: could not open file for read.\n");
-    	exit(1);
-    }
-
-
-	int result = 0;
-	int current_id = -1;
-	int current_followers = 0;
-	int max_followers = 0;
-	int total_edges = 0;
-    int unique_users = 0;
-	while((result = fread (buffer, sizeof(Record), records_per_block, fp_read)) > 0){
-		int i = 0;
-		total_records++;
-		
-		if (result != records_per_block){
-			records_per_block = result;
-		};
-
-		while(i < records_per_block){
-			total_edges += 1;
-			if (current_id == -1){
-				current_id = buffer[i].uid2;
-				current_followers = 1;
-				unique_users = 1;
-			}else if (current_id == buffer[i].uid2){
-				current_followers += 1;
-			} else {
-				current_id = buffer[i].uid2;
-				if (max_followers < current_followers){
-					max_followers = current_followers;
-				};
-				current_id = buffer[i].uid2;
-				current_followers = 1;
-				unique_users += 1;
-			};
-			i += 1;
-			
-		};
-
-	};
-	fclose(fp_read);
-	free(buffer);
-	return max_followers;
-
-}
-int compute_max_out_degree(char *filename, int block_size) {
-	int records_per_block = block_size/sizeof(Record);
-	FILE *fp_read;
-
-   long total_records = 0;
-
-	Record * buffer = (Record *) calloc (records_per_block, sizeof(Record));
-	if (!(fp_read = fopen(filename, "rb"))) {
-    	printf("Error: could not open file for read.\n");
-    	exit(1);
-    }
-
-
-	int result = 0;
-	int current_id = -1;
-	int current_followers = 0;
-	int max_followers = 0;
-	int total_edges = 0;
-    int unique_users = 0;
-	while((result = fread (buffer, sizeof(Record), records_per_block, fp_read)) > 0){
-		int i = 0;
-		total_records++;
-		
-		if (result != records_per_block){
-			records_per_block = result;
-		};
-
-		while(i < records_per_block){
-			total_edges += 1;
-			if (current_id == -1){
-				current_id = buffer[i].uid1;
-				current_followers = 1;
-				unique_users = 1;int max_degree = atoi(argv[4]);
-			}else if (current_id == buffer[i].uid1){
-				current_followers += 1;
-			} else {
-				current_id = buffer[i].uid1;
-				if (max_followers < current_followers){
-					max_followers = current_followers;
-				};
-				current_id = buffer[i].uid1;
-				current_followers = 1;
-				unique_users += 1;
-			};
-			i += 1;
-			
-		};
-
-	};
-	fclose(fp_read);
-	free(buffer);
-	return max_followers;
-}
-void distribution(char * filename, int block_size, char * column_id, int max_degree) {
+void distribution(char * filename, int block_size, int column_id, int max_degree) {
 	int records_per_block = block_size/sizeof(Record);
 	FILE *fp_read;
 	// allocate array 
-	degree_count[max_degree + 1];
+	int degree_count[max_degree + 1];
 	
 	Record * buffer = (Record *) calloc (records_per_block, sizeof(Record));
 	if (!(fp_read = fopen(filename, "rb"))) {
     	printf("Error: could not open file for read.\n");
     	exit(1);
     }
-    int i;
+
+   int result;
+   int i;
    int id;
    int count = 0;
    int flag = 1;
@@ -174,16 +56,142 @@ void distribution(char * filename, int block_size, char * column_id, int max_deg
 			};
 		}
 	}
+
 	for (i = 1; i < max_degree; i++) {
 		if (column_id == 1) {
-			printf("out degree of %d has count %d", i, degree_count[i]);
+			printf("out degree of %d has count %d\n", i, degree_count[i]);
 		} else {
-			printf("in degree of %d has count %d", i, degree_count[i]);
+			printf("in degree of %d has count %d\n", i, degree_count[i]);
 		}
 		
 	}
+
 	free(buffer);
-   fp_close(fp_read);
+    fclose(fp_read);
 	//return degree_count;
 }
+
+int main(int argc, char *argv[]){
+	char *filename = argv[1];
+	int block_size = atoi(argv[2]);
+	int column_id = atoi(argv[3]);
+	int max_degree;
+	if (column_id == 1) {
+		max_degree = compute_max_out_degree(filename, block_size);
+	} else {
+		max_degree = compute_max_in_degree(filename, block_size);
+	}
+	distribution(filename, block_size, column_id, max_degree);
+	return 0;
+}
+
+int compute_max_in_degree(char *filename, int block_size) {
+	int records_per_block = block_size/sizeof(Record);
+	FILE *fp_read;
+
+   long total_records = 0;
+
+	Record * buffer = (Record *) calloc (records_per_block, sizeof(Record));
+	if (!(fp_read = fopen(filename, "rb"))) {
+    	printf("Error: could not open file for read.\n");
+    	exit(1);
+    }
+
+
+	int result = 0;
+	int current_id = -1;
+	int current_followers = 0;
+	int max_followers = 0;
+	int total_edges = 0;
+    int unique_users = 0;
+
+	while((result = fread (buffer, sizeof(Record), records_per_block, fp_read)) > 0){
+		int i = 0;
+		total_records++;
+		
+		if (result != records_per_block){
+			records_per_block = result;
+		};
+
+		while(i < records_per_block){
+			total_edges += 1;
+			if (current_id == -1){
+				current_id = buffer[i].UID2;
+				current_followers = 1;
+				unique_users = 1;
+			}else if (current_id == buffer[i].UID2){
+				current_followers += 1;
+			} else {
+				current_id = buffer[i].UID2;
+				if (max_followers < current_followers){
+					max_followers = current_followers;
+				};
+				current_id = buffer[i].UID2;
+				current_followers = 1;
+				unique_users += 1;
+			};
+			i += 1;
+			
+		};
+
+	};
+	fclose(fp_read);
+	free(buffer);
+	return max_followers;
+}
+
+int compute_max_out_degree(char *filename, int block_size) {
+	int records_per_block = block_size/sizeof(Record);
+	FILE *fp_read;
+
+   long total_records = 0;
+
+	Record * buffer = (Record *) calloc (records_per_block, sizeof(Record));
+	if (!(fp_read = fopen(filename, "rb"))) {
+    	printf("Error: could not open file for read.\n");
+    	exit(1);
+    }
+
+
+	int result = 0;
+	int current_id = -1;
+	int current_followers = 0;
+	int max_followers = 0;
+	int total_edges = 0;
+    int unique_users = 0;
+	while((result = fread (buffer, sizeof(Record), records_per_block, fp_read)) > 0){
+		int i = 0;
+		total_records++;
+		
+		if (result != records_per_block){
+			records_per_block = result;
+		};
+
+		while(i < records_per_block){
+			total_edges += 1;
+			if (current_id == -1){
+				current_id = buffer[i].UID1;
+				current_followers = 1;
+				unique_users = 1;
+			}else if (current_id == buffer[i].UID1){
+				current_followers += 1;
+			} else {
+				current_id = buffer[i].UID1;
+				if (max_followers < current_followers){
+					max_followers = current_followers;
+				};
+				current_id = buffer[i].UID1;
+				current_followers = 1;
+				unique_users += 1;
+			};
+			i += 1;
+			
+		};
+
+	};
+	fclose(fp_read);
+	free(buffer);
+	return max_followers;
+}
+
 
